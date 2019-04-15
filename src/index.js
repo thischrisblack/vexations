e3b23edd9b8bd47143b5881e4c81332648a661ac
Chris Black <black@chrisblack.net>
2019-04-15 10:06:52 -0700

    Animation and time / rep displays.

--- src/index.js
+++ src/index.js
@@ -4,29 +4,62 @@ const part = document.querySelector('.part');
 
 let playing = false;
 
-let partMargin = screen.width / 5 * 4;
+let partMargin = screen.width / 2;
 
+let bpm = 46;
+
+let repetitions = 36;
+
+/**
+ * Find performance duration in seconds
+ * 52 = total beats in one repetition
+ * 840 = number of repetitions
+ */
+let performanceDuration = 52 * repetitions / bpm * 60;
+
+// Part div width in px
+let partWidth = 6800 * repetitions;
+
+// Part will scroll at this px/sec
+let scrollSpeed = partWidth / performanceDuration;
+
+var start = null;
 part.style.left = partMargin + 'px';
+part.style.width = partWidth + 'px';
 
-function startPlaying() {
-    playing = true;
-    var scroller = setInterval(function () {
-        partMargin -= 1;
-        part.style.left = partMargin + 'px';
-        if (playing === false) clearInterval(scroller);
-    }, 10);
+function scroll(timestamp) {
+    if (!start) start = timestamp;
+    var progress = timestamp - start;
+    var timeRemaining = msToTime((performanceDuration * 1000) - progress + 1000);
+    var repsCompleted = Math.floor(progress / (performanceDuration / repetitions * 1000));
+    part.style.transform = 'translateX(-' + (progress / (1000 / scrollSpeed)) + 'px)';
+    document.getElementById('timer').innerHTML = timeRemaining;
+    document.getElementById('counter').innerHTML = repsCompleted;
+    if (playing && repsCompleted < repetitions) {
+        window.requestAnimationFrame(scroll);
+    } else {
+        start = undefined;
+    }
 }
 
+function msToTime(duration) {
+    var seconds = parseInt((duration/1000)%60)
+        , minutes = parseInt((duration/(1000*60))%60)
+        , hours = parseInt((duration/(1000*60*60)));
+
+    // hours = (hours < 10) ? "0" + hours : hours;
+    // minutes = (minutes < 10) ? "0" + minutes : minutes;
+    // seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+    return hours + ' hour' + (hours !== 1 ? 's' : '') + ', ' + minutes + ' minute' + (minutes !== 1 ? 's' : '') + ', ' + seconds + ' seconds.';
+}
 
 window.addEventListener('keyup', e => {
-    if (e.code == 'Space') {
+    if (e.key == ' ') {
         if (!playing) {
-            playing = !playing;
-            startPlaying();
-        } else {
-            playing = !playing;
-            // clearInterval(scroller);
+            window.requestAnimationFrame(scroll);
         }
+        playing = !playing;
         
         console.log(playing);
     }
