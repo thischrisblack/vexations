434c57cd3171df57b0507a2b24352cb80e44856b
Chris Black <black@chrisblack.net>
2019-04-16 21:46:04 -0700

    STyles and click behavior.

--- src/index.js
+++ src/index.js
@@ -13,19 +13,31 @@ import numberer from './components/numberer';
 
     ui.update(performance);
 
-    numberer(performance);
+    // numberer(performance);
 
     ui.slider.oninput = function() {
         performance.tempo = ui.slider.value;
         ui.update(performance);
     }
 
-    window.addEventListener('keydown', e => {
-        if (e.key == ' ') {
-            performance.playing = !performance.playing;
-            if (performance.playing) {     
-                vexate(performance, music, ui);
+    var clicked = 0;
+
+    window.addEventListener('click', e => {
+        // If click is not on tempo slider
+        if (!e.target.closest('#tempo-slider')) {
+
+            if (clicked >= 2) {                
+                location.reload(false);
+            } else {
+                clicked++;
+                ui.slider.disabled = true;
+                performance.playing = !performance.playing;
+                if (performance.playing) {     
+                    vexate(performance, music, ui);
+                }
             }
+
+            
         }
     });
 
